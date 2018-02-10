56b7e3dc446f18cca84ad3ad82d5194261a3198e
NishmithaBalakrishna <35359310+NishmithaBalakrishna@users.noreply.github.com>
2018-02-11 00:14:24 +0530

    Update MovieList.java

--- demo/src/main/java/com/example/demo/controllers/MovieList.java
+++ demo/src/main/java/com/example/demo/controllers/MovieList.java
@@ -6,8 +6,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MovieList {
 
+	@JsonProperty("vote_average")
+	int voteAverage;
+	
+	public int getVoteAverage() {
+		return voteAverage;
+	}
+	public void setVoteAverage(int voteAverage) {
+		this.voteAverage = voteAverage;
+	}
+	private String status;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	int page;
 	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	@JsonProperty("total_results")
 	int totalResults;
 
